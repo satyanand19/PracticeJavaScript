6ed364c36d7f40209cefafbb60d7dd6d8d3ff03d
Satyanand Yadav <satyanand.yadav.19.1@mountblue.tech>
2022-03-29 07:46:16 +0530

    added settimeout

--- printTableOf2.js
+++ printTableOf2.js
@@ -1,3 +1,14 @@
-for(let a=1;a<=10;a++) {
-    console.log(`2 * ${a} = ${2*a}`);
+function printTable() {
+    for(let a=1;a<=10;a++) {
+        console.log(`2 * ${a} = ${2*a}`);
+    }
 }
+setTimeout(printTable,2);
+
+function Hi(){
+    console.log('Hi');
+    console.log('Hi');
+
+}
+
+setTimeout(Hi,2);
